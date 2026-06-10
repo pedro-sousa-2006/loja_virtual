a27bdc1ced745baabc73701e8ad0cb084b77cb70
pedro <ph.pedrosousa2006@gmail.com>
2026-06-10 10:30:51 -0300

    Criando tela inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TelaInicial } from "./tela-inicial/tela-inicial";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TelaInicial],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/tela-inicial/tela-inicial.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicial',
+  imports: [],
+  templateUrl: './tela-inicial.html',
+  styleUrl: './tela-inicial.css',
+})
+export class TelaInicial {
+
+}
